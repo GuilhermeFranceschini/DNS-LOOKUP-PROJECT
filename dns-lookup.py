4fd231f5492aa7042ef582b1a94645c0034b958c
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-21 15:56:29 -0300

    implementações

--- dns-lookup.py
+++ dns-lookup.py
@@ -1,8 +1,12 @@
 import dns.resolver
+domain = input("Digite o domínio que deseja consultar: ")
 def dnslook(domain, record_type):
-    domain = input("Digite o domínio que deseja consultar: ")
     answers = dns.resolver.resolve(domain, 'A')
     for answer in answers:
         print(f"Endereço IP: {answer.to_text()}")
-dnslook('example.com', 'A')
+dnslook(domain, 'A')
+dnslook(domain, 'AAAA')
+dnslook(domain, 'MX')
+dnslook(domain, 'NS')
+
 
