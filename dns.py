ebf59c27bf599606e65c4a37d59010e11a0be317
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-21 15:50:58 -0300

    Iniciei o codigo

---
+++ dns.py
@@ -0,0 +1,7 @@
+import dns.resolver
+def dnslook(domain, record_type):
+    domain = input("Digite o domínio que deseja consultar: ")
+    answers = dns.resolver.resolve(domain, 'A')
+    for answer in answers:
+        print(f"Endereço IP: {answer.to_text()}")
+
