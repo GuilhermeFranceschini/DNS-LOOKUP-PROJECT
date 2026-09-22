46611164e324c204fa0fc337a965950377acdcf2
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-22 16:33:38 -0300

    Aprimorei a saída de informações

--- dns-lookup.py
+++ dns-lookup.py
@@ -5,16 +5,16 @@ def dnslook(domain, record_type):
     try:
         answers = dns.resolver.resolve(domain, record_type)
         for answer in answers:
-            print(f"Resultado: {answer.to_text()}")
+            print(f"{record_type}: {answer.to_text()}")
     except dns.resolver.NXDOMAIN:
         print(f"{record_type}: Domínio não encontrado.")
     except dns.resolver.NoAnswer:
         print(f"{record_type}: Nenhum registro encontrado.")
-    print("Consulta concluída.\n")
+    
 
 dnslook(domain, 'A')
 dnslook(domain, 'AAAA')
 dnslook(domain, 'MX')
 dnslook(domain, 'NS')
-
+print("Consulta concluída.\n")
 
