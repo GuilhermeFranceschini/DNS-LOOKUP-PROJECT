ee1ca9d610cce2a633f8de23dc08eb38f5bcf34f
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-22 15:31:45 -0300

    Aprimorei o tratamento de erros

--- dns-lookup.py
+++ dns-lookup.py
@@ -1,9 +1,17 @@
 import dns.resolver
+print("Bem-vindo ao programa de consulta DNS!\n")
 domain = input("Digite o domínio que deseja consultar: ")
 def dnslook(domain, record_type):
-    answers = dns.resolver.resolve(domain, record_type)
-    for answer in answers:
-        print(f"Resultado: {answer.to_text()}")
+    try:
+        answers = dns.resolver.resolve(domain, record_type)
+        for answer in answers:
+            print(f"Resultado: {answer.to_text()}")
+    except dns.resolver.NXDOMAIN:
+        print(f"{record_type}: Domínio não encontrado.")
+    except dns.resolver.NoAnswer:
+        print(f"{record_type}: Nenhum registro encontrado.")
+    print("Consulta concluída.\n")
+
 dnslook(domain, 'A')
 dnslook(domain, 'AAAA')
 dnslook(domain, 'MX')
