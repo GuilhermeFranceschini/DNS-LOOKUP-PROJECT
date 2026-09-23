4dd55ec801cb11c68c3e2bee1fd01be764079aff
GuilhermeFranceschini <guilhermefranceschinijesuino8@gmail.com>
2026-09-23 17:09:56 -0300

    Melhorei a saída de dados

--- dns-lookup.py
+++ dns-lookup.py
@@ -13,24 +13,26 @@ domain = input("Digite o domínio que deseja consultar: ")
 if not domain:
     print("Nenhum domínio fornecido. Encerrando o programa.")
     exit()
-print("\nn1. Consultar registros A (IPv4)" \
-"\nn2. Consultar registros AAAA (IPv6)" \
-"\nn3. Consultar registros MX (Mail Exchange)" \
-"\nn4. Consultar registros NS (Name Server)" \
-"\nn5. Sair do programa")
-opcao = int(input("\nDigite o número da opção desejada: "))
-if opcao == 1:
-    dnslook(domain, 'A')
-if opcao == 2:
-    dnslook(domain, 'AAAA')
-if opcao == 3:
-    dnslook(domain, 'MX')
-if opcao == 4:
-    dnslook(domain, 'NS')
-if opcao == 5:
-    print("Saindo do programa.")
+while True:
+    print("\nn1. Consultar registros A (IPv4)" \
+    "\nn2. Consultar registros AAAA (IPv6)" \
+    "\nn3. Consultar registros MX (Mail Exchange)" \
+    "\nn4. Consultar registros NS (Name Server)" \
+    "\nn5. Sair do programa")
+    opcao = int(input("\nDigite o número da opção desejada: "))
+    if opcao == 1:
+        dnslook(domain, 'A')
+    if opcao == 2:
+        dnslook(domain, 'AAAA')
+    if opcao == 3:
+        dnslook(domain, 'MX')
+    if opcao == 4:
+        dnslook(domain, 'NS')
+    if opcao == 5:
+        print("Saindo do programa.")
     exit()
-if opcao < 1 or opcao > 5:
-    print("Opção inválida. Encerrando o programa.")
-print("Consulta concluída.\n")
+    if opcao < 1 or opcao > 5:
+        print("Opção inválida. Encerrando o programa.")
+        break
+    print("Consulta concluída.\n")
 
